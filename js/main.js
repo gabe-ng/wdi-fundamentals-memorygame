adb39e52cba8da16162a7377be3b184765e9dc08
Gabriel Ng <gabrielng15@gmail.com>
2018-05-26 23:01:51 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("Use flipped queen");
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else { alert("Sorry try again.") }
+}
